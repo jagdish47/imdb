36aa97c8f0bbe05d237603a47876224db9acb8a6
jagdish47 <jagdishkumawat81@gmail.com>
2023-10-14 17:33:07 +0530

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About Page</h1>
+
+      <p>
+        Introducing Cinephile Central, the ultimate movie application that takes
+        your cinematic experience to the next level. A seamless fusion of
+        innovation and homage to the world-renowned IMDb, Cinephile Central is
+        your one-stop destination for all things film-related.
+      </p>
+      <p>
+        Whether you're a seasoned cinephile or just looking for a captivating
+        night in, our IMDb clone offers a vast database of movies, TV shows, and
+        celebrities. Dive into a world of in-depth information, user reviews,
+        and ratings, allowing you to make informed choices and discover your
+        next cinematic masterpiece.
+      </p>
+      <p>
+        With a user-friendly interface, personalized watchlists, and real-time
+        updates on the latest releases, Cinephile Central ensures that every
+        movie lover finds their home in the world of cinema.
+      </p>
+      <p>
+        Explore, rate, and share your thoughts with a thriving community of
+        movie enthusiasts - because every great movie deserves its own fan club.
+        Welcome to Cinephile Central, where movies come to life!
+      </p>
+    </div>
+  );
+};
+
+export default About;
