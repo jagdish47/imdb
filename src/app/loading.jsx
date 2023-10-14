df81f55197f2e9aece0763efe8ec1732ef2598cf
jagdish47 <jagdishkumawat81@gmail.com>
2023-10-14 21:40:05 +0530

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <img className="h-56" src="spinner.svg" alt="loading-spinner" />
+    </div>
+  );
+};
+
+export default Loading;
